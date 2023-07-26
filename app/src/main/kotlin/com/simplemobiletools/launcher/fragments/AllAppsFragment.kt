5b318a4f599e7d3f5b197a64021446ff7b9f25dd
Ensar Sarajčić <dev@ensarsarajcic.com>
2023-07-26 18:23:56 +0200

    Check for `closeAppDrawer` config before closing it

--- app/src/main/kotlin/com/simplemobiletools/launcher/fragments/AllAppsFragment.kt
+++ app/src/main/kotlin/com/simplemobiletools/launcher/fragments/AllAppsFragment.kt
@@ -120,7 +120,9 @@ class AllAppsFragment(context: Context, attributeSet: AttributeSet) : MyFragment
             if (currAdapter == null) {
                 LaunchersAdapter(activity!!, launchers, this) {
                     activity?.launchApp((it as AppLauncher).packageName, it.activityName)
-                    activity?.closeAppDrawer()
+                    if (activity?.config?.closeAppDrawer == true) {
+                        activity?.closeAppDrawer()
+                    }
                     ignoreTouches = false
                     touchDownY = -1
                 }.apply {
