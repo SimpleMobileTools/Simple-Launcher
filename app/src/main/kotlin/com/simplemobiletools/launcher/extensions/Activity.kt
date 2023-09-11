ab8164ccdc2cb11f4fc755c122c8c3d4963d656b
Ensar Sarajčić <dev@ensarsarajcic.com>
2023-09-11 13:36:24 +0200

    Use long labels for shortcuts instead of short ones

--- app/src/main/kotlin/com/simplemobiletools/launcher/extensions/Activity.kt
+++ app/src/main/kotlin/com/simplemobiletools/launcher/extensions/Activity.kt
@@ -120,7 +120,7 @@ fun Activity.handleGridItemPopupMenu(anchorView: View, gridItem: HomeScreenGridI
         if (hasShortcuts) {
             val iconSize = resources.getDimensionPixelSize(R.dimen.menu_icon_size)
             shortcuts?.forEach {
-                menu.add(R.id.group_shortcuts, Menu.NONE, Menu.NONE, it.shortLabel)
+                menu.add(R.id.group_shortcuts, Menu.NONE, Menu.NONE, it.longLabel)
                     .setIcon(
                         launcherApps.getShortcutIconDrawable(it, resources.displayMetrics.densityDpi).toBitmap(width = iconSize, height = iconSize)
                             .toDrawable(resources)
